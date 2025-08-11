6f9bbaeb7bd3b6d9b7755b62863001250f1adbd5
MrFla <rgarciamoreno@gadir-consultores.com>
2025-08-11 16:42:34 +0200

    gg

--- ParkingMartiAPP/Repositorio/IRepositorio.cs
+++ ParkingMartiAPP/Repositorio/IRepositorio.cs
@@ -9,6 +9,45 @@ namespace ParkingMartiAPP.Repositorio
         public Task<Clientes> GetClienteID(int clienteID);
         public Task<Clientes> CrearCliente(Clientes cliente);
         public Task<Clientes> ActualizarCliente(int clienteID,Clientes cliente);
-        public Task<Clientes> EliminarCliente(int clienteID);
+        public Task EliminarCliente(int clienteID);
+
+        // Proveedores
+        public Task<List<Proveedores>> GetProveedores();
+        public Task<Proveedores> GetProveedorID(int proveedorID);
+        public Task<Proveedores> CrearProveedor(Proveedores proveedor);
+        public Task<Proveedores> ActualizarProveedor(int proveedorID, Proveedores proveedor);
+        public Task EliminarProveedor(int proveedorID);
+
+        // Trabajadores
+        public Task<List<Trabajadores>> GetTrabajadores();
+        public Task<Trabajadores> GetTrabajadorID(int trabajadorID);
+        public Task<Trabajadores> CrearTrabajador(Trabajadores proveedor);
+        public Task<Trabajadores> ActualizarTrabajador(int trabajadorID, Trabajadores trabajador);
+        public Task EliminarTrabajador(int trabajadorID);
+
+        // Camiones
+        public Task<List<Camiones>> GetCamiones();
+        public Task<Trabajadores> GetCamionID(int camionID);
+        public Task<Trabajadores> CrearCamion(Camiones camion);
+        public Task<Trabajadores> ActualizarCamion(int camionID, Camiones camion);
+        public Task EliminarCamion(int camionID);
+
+        // Proformas
+        public Task<List<Proformas>> GetProformas();
+        public Task<Proformas> GetProformaID(int proformaID);
+        public Task<Proformas> GetProformasPorFecha(DateTime fechaDesde, DateTime fechaHasta);
+
+        public Task<Proformas> CrearProforma(Proformas proforma);
+        public Task<Proformas> ActualizarProforma(int proformaID, Proformas proforma);
+        public Task EliminarProforma(int proformaID);
+
+        // Proformas
+        public Task<List<Proformas>> GetFacturas();
+        public Task<Proformas> GetFacturaID(int facturaID);
+        public Task<Proformas> GetFacturasPorFecha(DateTime fechaDesde, DateTime fechaHasta);
+
+        public Task<Proformas> CrearFactura(Proformas proforma,Facturas factura);
+        public Task<Proformas> AnularFactura(int facturaID, Proformas proforma);
+
     }
 }
---
+++ ParkingMartiAPP/Repositorio/Repositorio.cs
@@ -0,0 +1,196 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkingMartiAPP.Clases;
+using ParkingMartiAPP.Data;
+
+namespace ParkingMartiAPP.Repositorio
+{
+    public class Repositorio : IRepositorio
+    {
+        private readonly ApplicationDbContext _contexto;
+        public Repositorio (ApplicationDbContext contexto) 
+        {
+            _contexto = contexto;
+        }
+        public Task<Trabajadores> ActualizarCamion(int camionID, Camiones camion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Clientes> ActualizarCliente(int clienteID, Clientes cliente)
+        {
+            var clienteBD = await _contexto.Clientes.FindAsync(clienteID);
+            clienteBD.NIF = cliente.NIF;
+            clienteBD.CuentaBancaria = cliente.CuentaBancaria;
+            clienteBD.Correo = cliente.Correo;
+            clienteBD.Direccion = cliente.Direccion;
+            clienteBD.Camiones = cliente.Camiones;
+            clienteBD.Telefono2 = cliente.Telefono2;
+            clienteBD.Telefono1 = cliente.Telefono1;
+            clienteBD.NombreYApellidos = cliente.NombreYApellidos;
+            clienteBD.Poblacion = cliente.Poblacion;
+            await _contexto.SaveChangesAsync();
+            return clienteBD;
+        }
+
+        public Task<Proformas> ActualizarProforma(int proformaID, Proformas proforma)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Proveedores> ActualizarProveedor(int proveedorID, Proveedores proveedor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Trabajadores> ActualizarTrabajador(int trabajadorID, Trabajadores trabajador)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Proformas> AnularFactura(int facturaID, Proformas proforma)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Trabajadores> CrearCamion(Camiones camion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Clientes> CrearCliente(Clientes cliente)
+        {
+            if (cliente != null) 
+            {
+                await _contexto.Clientes.AddAsync(cliente);
+                await _contexto.SaveChangesAsync();
+                return cliente;
+            }
+            else 
+            {
+                return new Clientes();
+            }
+        }
+
+        public Task<Proformas> CrearFactura(Proformas proforma, Facturas factura)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Proformas> CrearProforma(Proformas proforma)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Proveedores> CrearProveedor(Proveedores proveedor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Trabajadores> CrearTrabajador(Trabajadores proveedor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Proveedores> EliminarCamion(int camionID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task EliminarCliente(int clienteID)
+        {
+            var clienteDB = await _contexto.Clientes.FindAsync(clienteID);
+            _contexto.Remove(clienteDB);
+            await _contexto.SaveChangesAsync();
+        }
+
+        public Task<Proformas> EliminarProforma(int proformaID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Proveedores> EliminarProveedor(int proveedorID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Proveedores> EliminarTrabajador(int trabajadorID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Camiones>> GetCamiones()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Trabajadores> GetCamionID(int camionID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Clientes> GetClienteID(int clienteID)
+        {
+            var clienteDB = await _contexto.Clientes.FindAsync(clienteID);
+            if (clienteDB == null) 
+            {
+                return new Clientes();
+            }
+            return clienteDB;
+        }
+
+        public Task<List<Clientes>> GetClientes()
+        {
+            return _contexto.Clientes.ToListAsync();
+        }
+
+        public Task<Proformas> GetFacturaID(int facturaID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Proformas>> GetFacturas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Proformas> GetFacturasPorFecha(DateTime fechaDesde, DateTime fechaHasta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Proformas> GetProformaID(int proformaID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Proformas>> GetProformas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Proformas> GetProformasPorFecha(DateTime fechaDesde, DateTime fechaHasta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Proveedores>> GetProveedores()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Proveedores> GetProveedorID(int proveedorID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Trabajadores>> GetTrabajadores()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Trabajadores> GetTrabajadorID(int trabajadorID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
